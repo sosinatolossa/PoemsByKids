ca91123aa632d0c78b196bd87ca3fc9403cba594
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-11 14:20:21 -0600

    initial commit

---
+++ SQLQuery4.sql
@@ -0,0 +1,14 @@
+﻿-- 1. How can we see the grades in the database?
+Select Name from Grade;
+
+-- 2. What emotions may be associated with a poem?
+Select Name from Emotion;
+
+-- 3. How many poems are in the database?
+Select Count (Id) From Poem;
+
+-- 4. Sort authors alphabetically by name.
+-- What are the names of the top 76 authors after authors are alphabetically ordered?
+Select Top (76) Name, Id From Author Order By Name;
+
+-- 5. 
